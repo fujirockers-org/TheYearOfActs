5234860ab20a101cef6642e9cb7c61eb1b83ae46
uji-rocker <uji.rocker@gmail.com>
2016-07-30 14:10:31 +0900

    first deploy

---
+++ chart.js
@@ -0,0 +1,67 @@
+$(function () {
+    var getCharts = function(title, data) {
+        return {
+            chart: {
+                type: 'column'
+            },
+            title: {
+	            text: title
+	        },
+	        subtitle: {
+	            text: ''
+	        },
+	        xAxis: {
+	            type: 'category'
+	        },
+	        yAxis: {
+	            title: {
+	                text: 'voting'
+	            }
+	
+	        },
+	        legend: {
+	            enabled: false
+	        },
+	        plotOptions: {
+	            series: {
+	                borderWidth: 0,
+	                dataLabels: {
+	                    enabled: true,
+	                    format: '{point.y:.1f}'
+	                }
+	            }
+	        },
+	
+	        tooltip: {
+	            headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+	            pointFormat: 'voted by <span style="color:{point.color}">{point.name}</span>: <b>{point.y:.1f}</b> rockers<br/>'
+	        },
+	
+	        series: [{
+	            name: 'Artist',
+	            colorByPoint: true,
+	            data: data
+	        }]
+	    }
+    };
+
+    $.getJSON("acts.json", function(d){
+    	var data = []
+        $.each(d.BestAct, function(i, v) {
+            data.push({name: i, y: v, drilldown: null});
+        })
+    	$('#container1').highcharts(getCharts("The Year of Best Acts 2016", data));
+
+    	data = []
+        $.each(d.GoodAct, function(i, v) {
+            data.push({name: i, y: v, drilldown: null});
+        })
+    	$('#container2').highcharts(getCharts("Good Acts 2016", data));
+
+    	data = []
+        $.each(d.WorstAct, function(i, v) {
+            data.push({name: i, y: v, drilldown: null});
+        })
+    	$('#container3').highcharts(getCharts("The Year of Worst Acts 2016", data));
+    })
+});
